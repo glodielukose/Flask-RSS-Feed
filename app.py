05300bf53ee5f584a3542a1d6f43693050131222
glodielukose <glodielukose@gmail.com>
2024-11-04 18:49:49 +0200

    add template directory

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import feedparser
 
 app = Flask(__name__)
 
+RSS_FEED = {
+  'bbc': "http://feeds.bbci.co.uk/news/rss.xml",
+  'cnn': 'http://rss.cnn.com/rss/edition.rss',
+  'fox': 'http://feeds.foxnews.com/foxnews/latest',
+  'iol': 'http://www.iol.co.za/cmlink/1.640'
+
+}
+
 @app.route("/")
-def index():
-  return "No news is good"
+def get_news():
+  query = request.args.get("publication")
+
+  if not query or query.lower() not in RSS_FEED:
+    publication = 'bbc'
+  else :
+    publication = query.lower()
+
+  feed = feedparser.parse(RSS_FEED[publication])
+
+  return render_template(
+    "home.html",
+    articles=feed['entries']
+    )
 
 if __name__ == "__main__":
   app.run()
